db3fa77eff451c90234f3c996a90701b29f696ce
Vaishali <vvyas@qa.com>
2021-08-07 22:32:16 +0100

    vv - 3rd Commit

--- src/main/java/com/natwest/library/LibraryApplication.java
+++ src/main/java/com/natwest/library/LibraryApplication.java
@@ -2,12 +2,22 @@ package com.natwest.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.natwest.library.repo.BooksRepo;
+
 
 @SpringBootApplication
 public class LibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
+		ApplicationContext ac = SpringApplication.run(LibraryApplication.class, args);
+		BooksRepo repo = ac.getBean(BooksRepo.class);
+		System.out.println(repo.findAllJPQL());
+		System.out.println(repo.findBooksByCategoryAndAuthor("Fiction", "Agatha Christie"));
+		System.out.println(repo.findBooksByReturnpolicyGreaterThan("10 Days"));
+	
 	}
+	
 
 }
